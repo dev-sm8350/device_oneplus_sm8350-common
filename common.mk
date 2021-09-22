bf01c9c5772a36c9276be9bbd57315472ff2d242
Alexander Winkowski <dereference23@outlook.com>
2021-09-22 17:31:50 +0200

    sm8350-common: Update sensors service to 2.1
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: Ieea074142aca0504b43805558ecf7fbd99c79d1f

--- common.mk
+++ common.mk
@@ -257,7 +257,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.frameworks.sensorservice@1.0 \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge \
     sensors.oplus
 
