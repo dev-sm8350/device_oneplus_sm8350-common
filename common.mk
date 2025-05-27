d7836b5b79b67e5fef7a0dd4714bd6e5fe54d2f0
TogoFire <togofire@mailfence.com>
2025-05-27 20:43:57 -0300

    sm8350-common: Nuke op cam
    Change-Id: 0fc89c4bf96f1e7b35a6783dcb5a66c43645fb3c
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- common.mk
+++ common.mk
@@ -98,8 +98,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti.recovery
 
 # Camera
-$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
